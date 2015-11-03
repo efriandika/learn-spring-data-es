65dd9119fe8dbe03a3ab72e525e6639d8065ff56
Efriandika Pratama <efriandika@gmail.com>
2015-11-03 10:40:14 +0700

    Init Spring Boot Apps

---
+++ src/main/java/com/efriandika/learn/LearnSpringDataEsApplication.java
@@ -0,0 +1,12 @@
+package com.efriandika.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDataEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringDataEsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/efriandika/LearnSpringDataEsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.efriandika;
+
+import com.efriandika.learn.LearnSpringDataEsApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LearnSpringDataEsApplication.class)
+public class LearnSpringDataEsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
